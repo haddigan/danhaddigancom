97e22e8f2d37ff19a1fdd00c22eabac8885bdc60
Dan <code@haddigan.email>
2024-06-04 16:16:19 -0400

    coerce return type of postById

--- api/query/postById.ts
+++ api/query/postById.ts
@@ -1,5 +1,13 @@
 import { Post } from "db";
 export default async function (postId: string) {
-  const post = { SK: "METADATA", PK: `POST#${postId}` };
-  return Post.get(post);
+  const postKey = { SK: "METADATA", PK: `POST#${postId}` };
+  return (await Post.get(postKey)).Item as {
+    id: string;
+    title: string | undefined;
+    caption: string | undefined;
+    imageUrl?: string | undefined;
+    created: string;
+    modified: string;
+    entity: string;
+  };
 }
--- app/routes/post.$postId.tsx
+++ app/routes/post.$postId.tsx
@@ -6,15 +6,10 @@ export default function Post() {
   const post = useLoaderData<typeof loader>();
 
   return (
-    // TODO: Figure out this alias issue
     <div>
-      {/* @ts-expect-error these aren't aliased like they should be */}
       <h1>{post.title}</h1>
-      {/* @ts-expect-error these aren't aliased like they should be */}
       <img src={post.imageUrl} alt={post.caption} />
-      {/* @ts-expect-error these aren't aliased like they should be */}
       <p>{post.caption}</p>
-
       <Form method="post" action="destroy">
         <button type="submit">Delete</button>
       </Form>
@@ -26,5 +21,5 @@ export const loader = async ({ params, request }: LoaderFunctionArgs) => {
   const postId = params.postId;
   if (!postId) throw new Error("No postId provided");
   const post = await client(request).postById.query(postId);
-  return json(post.Item);
+  return json(post);
 };
